4e5d11c3a626b38b22d72d9808dd34c31cfe24f6
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2025-07-25 09:23:04 +0200

    Implement void setup for leds and push-buttons

--- simon.ino
+++ simon.ino
@@ -4,6 +4,18 @@ const byte max_steps = 20;
 byte pattern[max_steps];
 byte patternLen = 0;
 
-void setup() {}
+void setup() {
+  // leds
+  pinMode(3, OUTPUT); // red
+  pinMode(4, OUTPUT); // yellow
+  pinMode(5, OUTPUT); // blue
+  pinMode(6, OUTPUT); // green
+
+  // push-buttons
+  pinMode(9, INPUT_PULLUP); // red
+  pinMode(10, INPUT_PULLUP); // yellow
+  pinMode(11, INPUT_PULLUP); // blue
+  pinMode(12, INPUT_PULLUP); // green
+}
 
 void loop() {}
